fc362c6c7a4e09da230b1d96f54b88f748ac36db
Andrew Start <andrewstart@cloudkid.com>
2014-08-08 15:07:20 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Animator",
+        "Animator.AnimTimeline",
+        "AssetManager",
+        "Button",
+        "DragManager",
+        "PixiDisplay",
+        "Positioner",
+        "ScreenSettings",
+        "UIElement",
+        "UIElementSettings",
+        "UIScaler"
+    ],
+    "modules": [
+        "cloudkid"
+    ],
+    "allModules": [
+        {
+            "displayName": "cloudkid",
+            "name": "cloudkid",
+            "description": "Initially layouts all interface elements"
+        }
+    ]
+} };
+});
